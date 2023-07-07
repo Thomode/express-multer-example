0a88042ec0c27ebcc52a645bab9e86889ef1a502
Tomas Figueroa Lopez <91357@sistemas.frc.utn.edu.ar>
2023-07-07 00:23:08 -0300

    agregando get de archivos

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,9 +1,17 @@
 const { Router } = require('express')
+const fs = require('fs')
 const uploadController = require('../controllers/upload.controller')
 
 const router = Router()
 
 router.post('', uploadController.upload.single('myFile'), uploadController.uploadFile)
+
+router.get('', async (req, res) => {
+    const files = fs.readdirSync('./src/public/upload')
+
+    console.log(files)
+    res.json({files})
+})
 
 const uploadRouter = {
     router
